cb77b7b91a62ca789e8b2eb7799950e5c6870626
gautamjain1009 <gautamjain1009@gmail.com>
2022-04-25 22:33:00 +0300

    adding helper functions for transformations

--- datasets/Apollo3d_loader.py
+++ datasets/Apollo3d_loader.py
@@ -5,7 +5,7 @@ import scipy
 import os 
 import glob
 import random
-
+from scipy.interpolate import interp1d
 import torchvision.transforms as transforms
 from torch.utils.data import Dataset, DataLoader
 import logging 
@@ -31,6 +31,103 @@ rho: [batch_size, N-tiles, 1]
 delta_z: [batch_size, N-tiles, 1]
 classidity_score: [batch_size, N-tiles, 1] or may be [batch_size, N-tiles, 2] as per the loss function used
 """
+
+#helper function later to add in the utils.py responsible for the projection 
+def homography_crop_resize(org_img_size, crop_y, resize_img_size):
+    """
+        compute the homography matrix transform original image to cropped and resized image
+    :param org_img_size: [org_h, org_w]
+    :param crop_y:
+    :param resize_img_size: [resize_h, resize_w]
+    :return:
+    """
+    # transform original image region to network input region
+    ratio_x = resize_img_size[1] / org_img_size[1]
+    ratio_y = resize_img_size[0] / (org_img_size[0] - crop_y)
+    H_c = np.array([[ratio_x, 0, 0],
+                    [0, ratio_y, -ratio_y*crop_y],
+                    [0, 0, 1]])
+    return H_c
+
+def projection_g2im(cam_pitch, cam_height, K):
+    P_g2c = np.array([[1,                             0,                              0,          0],
+                      [0, np.cos(np.pi / 2 + cam_pitch), -np.sin(np.pi / 2 + cam_pitch), cam_height],
+                      [0, np.sin(np.pi / 2 + cam_pitch),  np.cos(np.pi / 2 + cam_pitch),          0]])
+    P_g2im = np.matmul(K, P_g2c)
+    return P_g2im
+
+def homograpthy_g2im(cam_pitch, cam_height, K):
+    # transform top-view region to original image region
+    R_g2c = np.array([[1, 0, 0],
+                      [0, np.cos(np.pi / 2 + cam_pitch), -np.sin(np.pi / 2 + cam_pitch)],
+                      [0, np.sin(np.pi / 2 + cam_pitch), np.cos(np.pi / 2 + cam_pitch)]])
+    H_g2im = np.matmul(K, np.concatenate([R_g2c[:, 0:2], [[0], [cam_height], [0]]], 1))
+    return H_g2im
+
+def resample_laneline_in_y(input_lane, y_steps, out_vis=False):
+    """
+        Interpolate x, z values at each anchor grid, including those beyond the range of input lnae y range
+    :param input_lane: N x 2 or N x 3 ndarray, one row for a point (x, y, z-optional).
+                       It requires y values of input lane in ascending order
+    :param y_steps: a vector of steps in y
+    :param out_vis: whether to output visibility indicator which only depends on input y range
+    :return:
+    """
+
+    # at least two points are included
+    assert(input_lane.shape[0] >= 2)
+
+    y_min = np.min(input_lane[:, 1])-5
+    y_max = np.max(input_lane[:, 1])+5
+
+    if input_lane.shape[1] < 3:
+        input_lane = np.concatenate([input_lane, np.zeros([input_lane.shape[0], 1], dtype=np.float32)], axis=1)
+
+    f_x = interp1d(input_lane[:, 1], input_lane[:, 0], fill_value="extrapolate")
+    f_z = interp1d(input_lane[:, 1], input_lane[:, 2], fill_value="extrapolate")
+
+    x_values = f_x(y_steps)
+    z_values = f_z(y_steps)
+
+    if out_vis:
+        output_visibility = np.logical_and(y_steps >= y_min, y_steps <= y_max)
+        return x_values, z_values, output_visibility.astype(np.float32) + 1e-9
+    return x_values, z_values
+
+def transform_lane_g2gflat(h_cam, X_g, Y_g, Z_g):
+    """
+        Given X coordinates in flat ground space, Y coordinates in flat ground space, and Z coordinates in real 3D ground space
+        with projection matrix from 3D ground to flat ground, compute real 3D coordinates X, Y in 3D ground space.
+
+    :param P_g2gflat: a 3 X 4 matrix transforms lane form 3d ground x,y,z to flat ground x, y
+    :param X_gflat: X coordinates in flat ground space
+    :param Y_gflat: Y coordinates in flat ground space
+    :param Z_g: Z coordinates in real 3D ground space
+    :return:
+    """
+
+    X_gflat = X_g * h_cam / (h_cam - Z_g)
+    Y_gflat = Y_g * h_cam / (h_cam - Z_g)
+
+    return X_gflat, Y_gflat
+
+def homographic_transformation(Matrix, x, y):
+    """
+    Helper function to transform coordinates defined by transformation matrix
+
+    Args:
+            Matrix (multi dim - array): 3x3 homography matrix
+            x (array): original x coordinates
+            y (array): original y coordinates
+    """
+    
+    ones = np.ones((1, len(y)))
+    coordinates = np.vstack((x, y, ones))
+    trans = np.matmul(Matrix, coordinates)
+
+    x_vals = trans[0, :]/trans[2, :]
+    y_vals = trans[1, :]/trans[2, :]
+    return x_vals, y_vals
 
 #create a pytorch dataset class
 class Apollo3d_loader(Dataset):
@@ -66,6 +163,7 @@ class Apollo3d_loader(Dataset):
         """
     def load_dataset(self):
 
+        #TODO: Make it more efficient 
         if not os.path.exists(self.data_filepath):
             raise Exception('Fail to load the train.json file')
 
@@ -148,12 +246,20 @@ class Apollo3d_loader(Dataset):
         batch.update({'gt_lanelines':gt_lanelines})
  
         #TODO: add transforms and update the other data accordingly
+        
         #convert the image to tensor
         img = transforms.ToTensor()(img)
         img = img.float()
         batch.update({"image":img})
         
         # return img, gt_camera_height, gt_camera_pitch, gt_lanelines
+
+
+        """
+        TODO: Other Inputs I need to add here except than that,
+
+
+        """
         return batch
         
 def collate_fn(batch):
@@ -164,9 +270,9 @@ def collate_fn(batch):
     img_data = [item['image'] for item in batch]
     img_data = torch.stack(img_data, dim = 0)
     
-    gt_camera_height_data = [item[1] for item in batch]
-    gt_camera_pitch_data = [item[2] for item in batch]
-    gt_lanelines_data = [item[3] for item in batch] #need to check the data representation of lanelines (vis)
+    gt_camera_height_data = [item['gt_height'] for item in batch]
+    gt_camera_pitch_data = [item['gt_pitch'] for item in batch]
+    gt_lanelines_data = [item['gt_lanelines'] for item in batch] #need to check the data representation of lanelines (vis)
 
     return [img_data, gt_camera_height_data, gt_camera_pitch_data, gt_lanelines_data]
 
@@ -199,6 +305,7 @@ def collate_fn(batch):
 
 
 if __name__ == "__main__":
+
     #intialize the dataset
     data_root = '/home/gautam/e2e/lane_detection/3d_approaches/3d_dataset/Apollo_Sim_3D_Lane_Release'
     data_splits = '/home/gautam/e2e/lane_detection/3d_approaches/3d_dataset/3D_Lane_Synthetic_Dataset/old_data_splits/standard'
@@ -215,7 +322,7 @@ if __name__ == "__main__":
         # print(data['gt_lanelines'].size())
         # print(data['gt_height'])
         # print(data['gt_pitch'])
-        # print(data[0].shape)
+        print(data[0].shape)
         # print(data[1])
         # print(data[2])
         print(len(data[3][0][1]))## index ,batch_size, i == lane_cnt, data_points
