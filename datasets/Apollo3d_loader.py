4889cb38fbe0706460bba4845801bcbf8a7c5bcb
gautamjain1009 <gautamjain1009@gmail.com>
2022-05-12 18:17:01 +0300

    moving helper functions for dataloader

--- datasets/Apollo3d_loader.py
+++ datasets/Apollo3d_loader.py
@@ -1,211 +1,20 @@
-from curses import resize_term
+import sys
+sys.path.append("../")
 import torch 
 import numpy as np 
 import cv2 
-import scipy 
 import os 
-import glob
-import random
-from scipy.interpolate import interp1d
 import torchvision.transforms as transforms
-import torchvision.transforms.functional as F
 from torch.utils.data import Dataset, DataLoader
 import logging 
 logging.basicConfig(level = logging.DEBUG)
 import json 
-import sys
+from utils.helper_functions import *
 
-sys.path.append("../")
 """
 Import just for unit test
 """
 from utils.config import Config
-
-#helper function later to add in the utils.py responsible for the projection 
-def homography_crop_resize(org_img_size, crop_y, resize_img_size):
-    """
-        compute the homography matrix transform original image to cropped and resized image
-    :param org_img_size: [org_h, org_w]
-    :param crop_y:
-    :param resize_img_size: [resize_h, resize_w]
-    :return:
-    """
-    # transform original image region to network input region
-    ratio_x = resize_img_size[1] / org_img_size[1]
-    ratio_y = resize_img_size[0] / (org_img_size[0] - crop_y)
-    H_c = np.array([[ratio_x, 0, 0],
-                    [0, ratio_y, -ratio_y*crop_y],
-                    [0, 0, 1]])
-    return H_c
-
-def projection_g2im(cam_pitch, cam_height, K):
-    P_g2c = np.array([[1,                             0,                              0,          0],
-                      [0, np.cos(np.pi / 2 + cam_pitch), -np.sin(np.pi / 2 + cam_pitch), cam_height],
-                      [0, np.sin(np.pi / 2 + cam_pitch),  np.cos(np.pi / 2 + cam_pitch),          0]])
-    P_g2im = np.matmul(K, P_g2c)
-    return P_g2im
-
-def homography_g2im(cam_pitch, cam_height, K):
-    # transform top-view region to original image region
-    R_g2c = np.array([[1, 0, 0],
-                      [0, np.cos(np.pi / 2 + cam_pitch), -np.sin(np.pi / 2 + cam_pitch)],
-                      [0, np.sin(np.pi / 2 + cam_pitch), np.cos(np.pi / 2 + cam_pitch)]])
-    H_g2im = np.matmul(K, np.concatenate([R_g2c[:, 0:2], [[0], [cam_height], [0]]], 1))
-    return H_g2im
-
-def resample_laneline_in_y(input_lane, y_steps, out_vis=False):
-    """
-        Interpolate x, z values at each anchor grid, including those beyond the range of input lnae y range
-    :param input_lane: N x 2 or N x 3 ndarray, one row for a point (x, y, z-optional).
-                       It requires y values of input lane in ascending order
-    :param y_steps: a vector of steps in y
-    :param out_vis: whether to output visibility indicator which only depends on input y range
-    :return:
-    """
-
-    # at least two points are included
-    assert(input_lane.shape[0] >= 2)
-
-    y_min = np.min(input_lane[:, 1])-5
-    y_max = np.max(input_lane[:, 1])+5
-
-    if input_lane.shape[1] < 3:
-        input_lane = np.concatenate([input_lane, np.zeros([input_lane.shape[0], 1], dtype=np.float32)], axis=1)
-
-    f_x = interp1d(input_lane[:, 1], input_lane[:, 0], fill_value="extrapolate")
-    f_z = interp1d(input_lane[:, 1], input_lane[:, 2], fill_value="extrapolate")
-
-    x_values = f_x(y_steps)
-    z_values = f_z(y_steps)
-
-    if out_vis:
-        output_visibility = np.logical_and(y_steps >= y_min, y_steps <= y_max)
-        return x_values, z_values, output_visibility.astype(np.float32) + 1e-9
-    return x_values, z_values
-
-def transform_lane_g2gflat(h_cam, X_g, Y_g, Z_g):
-    """
-        Given X coordinates in flat ground space, Y coordinates in flat ground space, and Z coordinates in real 3D ground space
-        with projection matrix from 3D ground to flat ground, compute real 3D coordinates X, Y in 3D ground space.
-
-    :param P_g2gflat: a 3 X 4 matrix transforms lane form 3d ground x,y,z to flat ground x, y
-    :param X_gflat: X coordinates in flat ground space
-    :param Y_gflat: Y coordinates in flat ground space
-    :param Z_g: Z coordinates in real 3D ground space
-    :return:
-    """
-
-    X_gflat = X_g * h_cam / (h_cam - Z_g)
-    Y_gflat = Y_g * h_cam / (h_cam - Z_g)
-
-    return X_gflat, Y_gflat
-
-def homographic_transformation(Matrix, x, y):
-    """
-    Helper function to transform coordinates defined by transformation matrix
-
-    Args:
-            Matrix (multi dim - array): 3x3 homography matrix
-            x (array): original x coordinates
-            y (array): original y coordinates
-    """
-    
-    ones = np.ones((1, len(y)))
-    coordinates = np.vstack((x, y, ones))
-    trans = np.matmul(Matrix, coordinates)
-
-    x_vals = trans[0, :]/trans[2, :]
-    y_vals = trans[1, :]/trans[2, :]
-    return x_vals, y_vals
-
-def deg2rad(angle):
-    
-    #convert angle from degree to radian
-    a = (np.pi / 180) 
-    b = angle * a
-
-    return b 
-
-def binprob(n_bin, angle, d2rad = False):
-    
-    """
-    helper function to generate the bin prob vector
-    (phi) from line angle for each tile
-    
-    """
-    constant = 2*np.pi/n_bin
-    
-    if d2rad == True: 
-        rad_angle = deg2rad(angle)
-    else:
-        rad_angle = angle
-
-    bin_vector = np.zeros((n_bin, 1))
-    
-    for i in range(n_bin):
-        p_aplha = 1 - (abs((constant * i) - rad_angle)/constant)
-        bin_vector[i] = p_aplha
-
-    bin_vector[bin_vector<0] = 0
-
-    return bin_vector 
-
-def HoughLine(image):
-        ''' Basic Hough line transform that builds the accumulator array
-        Input : image tile (gray scale image)
-        Output : accumulator : the accumulator of hough space
-                thetas : values of theta (0 : 360)
-                rs : values of radius (-max distance : max distance)
-        '''
-        lane_exist = False
-
-        #Get image dimensions
-        # y for rows and x for columns 
-        Ny = image.shape[0]
-        Nx = image.shape[1]
-
-        #Max diatance is diagonal one 
-        Maxdist = int(np.round(np.sqrt(Nx**2 + Ny ** 2)))
-
-        # Theta in range from -90 to 90 degrees
-        thetas = np.deg2rad(np.arange(0, 360))
-        
-        #Range of radius
-        rs = np.linspace(-Maxdist, Maxdist, 2*Maxdist)
-
-        #Create accumulator array and initialize to zero
-        accumulator = np.zeros((2 * Maxdist, len(thetas)))
-         
-        # Loop for each edge pixel
-        for y in range(Ny):
-            for x in range(Nx):
-                # Check if it is an edge pixel
-                #  NB: y -> rows , x -> columns
-                if image[y,x] > 0:
-                    lane_exist = True
-                    #Loop for each theta
-                    # Map edge pixel to hough space
-                    for k in range(len(thetas)):
-                    #calculate ρ
-                        r = x*np.cos(thetas[k]) + y * np.sin(thetas[k])
-                    # Increment accumulator at r, theta    
-                        # Update the accumulator
-                        # N.B: r has value -max to max
-                        # map r to its idx 0 : 2*max
-                        accumulator[int(r) + Maxdist,k] += 1
-        return accumulator, thetas, rs, lane_exist
-
-def data_aug_rotate(img):
-    
-    rot = random.uniform(-np.pi/18, np.pi/18)
-    # rot = random.uniform(-10, 10)
-    center_x = img.shape[0]/ 2
-    center_y = img.shape[1]/ 2
-    rot_mat = cv2.getRotationMatrix2D((center_x, center_y), rot, 1.0)
-    img_rot = np.array(img)
-    img_rot = cv2.warpAffine(img_rot, rot_mat, (img.shape[0],img.shape[1]), flags=cv2.INTER_LINEAR)
-    rot_mat = np.vstack([rot_mat, [0, 0, 1]])
-    return img_rot, rot_mat
 
 class CalculateDistanceAngleOffests(object):
     def __init__(self, org_h, org_w, resize_h, resize_w, K, ipm_w, ipm_h, crop_y, top_view_region):
@@ -358,7 +167,7 @@ def generategt_pertile(gt_lanes, img , gt_cam_height, gt_cam_pitch, cfg):
     
     bev_projected_lanes, dz_dict = calculate_bev_projection.draw_bevlanes(gt_lanes, img,  gt_cam_height, gt_cam_pitch, color_list) ## returns an image array of gt lanes projected on BEV
 
-    cv2.imwrite("/home/gautam/Thesis/E2E_3DLane_AuxNet/datasets/complete_lines_test.jpg" , bev_projected_lanes)
+    # cv2.imwrite("/home/gautam/Thesis/E2E_3DLane_AuxNet/datasets/complete_lines_test.jpg" , bev_projected_lanes)
 
     ##init gt arrays for rho, phi and classification score
     gt_rho = np.zeros((int(bev_projected_lanes.shape[0]/tile_size), int(bev_projected_lanes.shape[1]/tile_size)))
@@ -437,7 +246,6 @@ def generategt_pertile(gt_lanes, img , gt_cam_height, gt_cam_pitch, cfg):
 
     return gt_rho, gt_phi, gt_c, gt_lane_class, gt_delta_z
 
-#TODO: add data augmentation as per the GeoNet paper
 class Apollo3d_loader(Dataset):
     def __init__(self, data_root, data_splits, phase = "train", cfg = None):
         super(Apollo3d_loader, self,).__init__()
@@ -459,7 +267,6 @@ class Apollo3d_loader(Dataset):
         
     def load_dataset(self):
 
-        #TODO: Make it more efficient
         if not os.path.exists(self.data_filepath):
             raise Exception('Fail to load the train.json file')
 
